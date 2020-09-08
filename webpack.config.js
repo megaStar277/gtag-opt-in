a465492e3cec9d3e837947c78f8ec46bdc785a30
solarspecter315 <ted563473@gmail.com>
2020-09-08 08:43:28 +0200

    Update webpack.config.js to export only UMD

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,6 @@ module.exports = [{
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'GTagOptIn',
-    libraryTarget: 'var'
-  },
-  mode: 'production'
-},{
-  entry: './src/index.js',
-  output: {
-    filename: 'index.umd.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'GTagOptIn',
     libraryTarget: 'umd'
   },
   mode: 'production'
